f51ff9fcc16841793dbfae1f20e99ea149f11dc6
Pie <95382349+Asmuri@users.noreply.github.com>
2022-12-23 00:34:08 +0100

    Commit

---
+++ src/main/java/com/webapplication/gamespring/MainTest.java
@@ -0,0 +1,24 @@
+package com.webapplication.gamespring;
+
+import com.webapplication.gamespring.model.Utente;
+import com.webapplication.gamespring.persistenza.Dao.UtenteDao;
+import com.webapplication.gamespring.persistenza.Dao.postgres.UtenteDaoPostgres;
+import com.webapplication.gamespring.persistenza.DatabaseManager;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+
+        Utente U = new Utente();
+        U.setUsername("Pie_Ox");
+        U.setPassword("123456");
+        U.setEmail("piero.stalteri@icloud.com");
+        U.setBandito(false);
+        U.setAmministratore(true);
+
+
+        UtenteDao udao = DatabaseManager.getInstance().getUtenteDao();
+        udao.saveOrUpdate(U);
+
+    }
+}
