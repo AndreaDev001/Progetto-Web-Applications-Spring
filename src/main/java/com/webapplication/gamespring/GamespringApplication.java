6f68f1fc3f97c49d04422d684e4061b4115aa12f
PierKnight <pier.altimari@libero.it>
2022-12-22 11:57:04 +0100

    first commit

---
+++ src/main/java/com/webapplication/gamespring/GamespringApplication.java
@@ -0,0 +1,13 @@
+package com.webapplication.gamespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamespringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapplication/gamespring/GamespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapplication.gamespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
