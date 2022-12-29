250b0a6fde5604c7425ec2f77529622ffe698ffd
francescafilice <francescafilice.ff@libero.it>
2022-12-29 11:10:29 +0100

    Completato validazione registrazione lato server

--- src/main/java/com/webapplication/gamespring/controller/RegistrationHandler.java
+++ src/main/java/com/webapplication/gamespring/controller/RegistrationHandler.java
@@ -3,6 +3,7 @@ package com.webapplication.gamespring.controller;
 import com.webapplication.gamespring.model.Utente;
 import com.webapplication.gamespring.persistenza.Dao.UtenteDao;
 import com.webapplication.gamespring.persistenza.DatabaseManager;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,38 +22,59 @@ public class RegistrationHandler {
         String username =  body.get("username");
         String password =  body.get("password");
 
+        // controlla che i campi NON siano vuoti
+        if (email.isEmpty() || email.isBlank() ||
+                username.isEmpty() || username.isBlank() ||
+                password.isEmpty() || password.isBlank() ) {
+            return "emptyFields";
+        }
 
-
-        // todo: trim email e username
+        // trim email e username
         email = email.trim();
         username = username.trim();
 
-        // todo: rifai validazione params lato server
+        // validazione params lato server
         // email regex
+        if (!email.matches("^[a-zA-Z\\d_+&*-\\/]+(?:\\.[a-zA-Z\\d_+&*-]+)*@(?:[a-zA-Z\\d-]+\\.)+[a-zA-Z]{2,7}$")) {
+            System.out.println(email + " Invalid email");
+            return "invalidEmail";
+        }
+
         // username min 3 max 50 char
-        // pw con min 1 numero, 1 minuscola, 1 maiuscola,  char speciale, min 8 char di lunghezza
+        if (username.length() < 3 || username.length() > 50) {
+            return "invalidUsername";
+        }
+
+        // pw con min 1 numero, 1 minuscola, 1 maiuscola, char speciale, min 8 char di lunghezza
+        if (password.length() < 8 ||
+                !password.matches(".*[a-z].*") ||
+                !password.matches(".*[A-Z].*") ||
+                !password.matches(".*\\d.*") ||
+                !password.matches(".*[^\\s\\d\\w].*") ||
+                password.contains(" ")) {
+            return "invalidPassword";
+        }
 
 
-
-        // todo: check username inesistente nel db
+        // check username inesistente nel db
         UtenteDao uDao = DatabaseManager.getInstance().getUtenteDao();
         Utente utente = uDao.findByPrimaryKey(username);
         // se esiste già un utente con questo username nel DB:
         if (utente != null) {
-            return "invalidUsername";
+            return "unavailableUsername";
         }
 
-        // todo: check email inesistente nel db
+        // check email inesistente nel db
         List<Utente> utenti = uDao.findAll();
         for (Utente u : utenti) {
             if (u.getEmail().equals(email))
-                return "invalidEmail";
+                return "unavailableEmail";
         }
 
-        // todo: cripta pw
+        // cripta pw
         String encryptedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
 
-        // todo: crea utente e salvalo nel db
+        // crea utente e salvalo nel db
         Utente newUtente = new Utente();
         newUtente.setEmail(email);
         newUtente.setUsername(username);
