3dd06b12792d82102b342569668f37d293bdba7a
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-01-26 14:53:33 +0100

    Fix

--- src/main/resources/static/js/wishlist.js
+++ src/main/resources/static/js/wishlist.js
@@ -1,6 +1,7 @@
 let wishlistCards = document.querySelectorAll(".card")
 function removeWishlistEntry(event, value)
 {
+    console.log(value);
     let element = document.getElementById(value);
     element.delete();
 }
