47bb1d2b3fef2aca8cf07ea99607afcd650eb1fe
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-01-26 16:29:53 +0100

    Fix wishlist

--- src/main/resources/static/js/wishlist.js
+++ src/main/resources/static/js/wishlist.js
@@ -1,11 +1,20 @@
-
+$(document).ready(() => updateValues());
+function updateValues()
+{
+    let card = document.getElementsByClassName("card");
+    let missingGameDiv = document.getElementById("noGameDiv");
+    let gameHolder = document.getElementById("gameHolder");
+    missingGameDiv.style.display = card.length > 0 ? "none" : "block";
+    gameHolder.style.display = card.length > 0 ? "flex" : "none";
+    console.log(missingGameDiv);
+}
 function removeWishlistEntry(value)
 {
-
     $.post("http://localhost:8080/removeGameWishlistSpring", {gameID: value}
     ).done(function() {
         document.querySelector(`div[gameid="${value}"]`).remove();
         alert("Wishlist entry successfully removed");
+        updateValues();
     });
 
 }
