bd5e3244cb7840d90b6395e2c22009607ca21db6
PierKnight <pier.altimari@libero.it>
2023-02-03 15:48:40 +0100

    added missing interface method

--- src/main/java/com/webapplication/gamespring/persistenza/Dao/CommentoDao.java
+++ src/main/java/com/webapplication/gamespring/persistenza/Dao/CommentoDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public interface CommentoDao {
 
+    List<Commento> getReviewComments(int reviewID, int startIndex, int commentsSize);
     Commento readCommento(ResultSet resultSet) throws SQLException; // permette di leggere il commento per evitare duplicazione del codice
     int save(Commento commento) throws SQLException; // salva i nuovi dati all'interno del database
     void update(Commento commento) throws SQLException; // aggiorna i dati all'interno del database
